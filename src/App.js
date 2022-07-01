7cb2dc017aca245641d695b309333f254a02c042
nordeeen <ahmadnrdnsah@gmail.com>
2022-07-01 00:12:43 -0700

    added code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -27,6 +28,13 @@ function App() {
       date: new Date(2022, 3, 21),
     },
   ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Lets get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
