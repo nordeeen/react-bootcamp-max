cee4d4d4e6306136367d1e5182889c4f5ab3546f
nordeeen <ahmadnrdnsah@gmail.com>
2022-07-05 22:54:44 +0700

    adding mapping list ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: "267.60",
-      date: new Date(2022, 2, 23),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance",
-      amount: "294.67",
-      date: new Date(2020, 7, 28),
-    },
-    {
-      id: "e3",
-      title: "Buy Ayam Geprek",
-      amount: "10.000",
-      date: new Date(2022, 5, 11),
-    },
-    {
-      id: "e4",
-      title: "Buy Smartphone",
-      amount: "1294.62",
-      date: new Date(2022, 3, 21),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: "267.60",
+    date: new Date(2022, 2, 23),
+  },
+  {
+    id: "e2",
+    title: "Car Insurance",
+    amount: "294.67",
+    date: new Date(2020, 7, 28),
+  },
+  {
+    id: "e3",
+    title: "Buy Ayam Geprek",
+    amount: "10.000",
+    date: new Date(2022, 5, 11),
+  },
+  {
+    id: "e4",
+    title: "Buy Smartphone",
+    amount: "1294.62",
+    date: new Date(2022, 3, 21),
+  },
+];
 
-  // return React.createElement(
-  //   "div",
-  //   {},
-  //   React.createElement("h2", {}, "Lets get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("in App.js");
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
     console.log(expense);
   };
 
